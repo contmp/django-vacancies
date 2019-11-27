491da91d920bdca04a793b413c8589437f5b555e
Bastian Probian <contmp@me.com>
2019-11-27 03:52:52 +0100

    Url config Harmonisation

--- vacancies/urls.py
+++ vacancies/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from vacancies.views import VacancyList, VacancyDetailView
 
 urlpatterns = [
-    path('vacancies/', VacancyList.as_view()),
-    path('vacancies/<slug:slug>/', VacancyDetailView.as_view(), name='vacancy-detail'),
+    path('', VacancyList.as_view()),
+    path('<slug:slug>/', VacancyDetailView.as_view(), name='vacancy-detail'),
 ]
